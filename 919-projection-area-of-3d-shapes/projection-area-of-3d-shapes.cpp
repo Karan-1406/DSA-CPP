c046f42a544e0d3b5a45dcb260d8951e36486655
Karan-1406 <karanpandey085@gmail.com>
2026-07-26 10:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (75.93%) - LeetSync

---
+++ 919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int projectionArea(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]!=0)ans++;
+            }
+        }
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                maxi=max(grid[i][j],maxi);
+            }
+            ans+=maxi;
+            maxi=INT_MIN;
+        }
+        //maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                maxi=max(maxi,grid[j][i]);
+            }
+            ans+=maxi;
+            maxi=INT_MIN;
+        }
+        return ans;
+    }
+};
