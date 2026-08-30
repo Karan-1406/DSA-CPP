1175985321f65a99fa1c36cd0951cab7fe84da87
Karan-1406 <karanpandey085@gmail.com>
2026-08-30 11:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.78%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -14,23 +14,38 @@ public:
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
         if(obstacleGrid[0][0] == 1) return 0;
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        //vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
        // return solve(m-1,n-1,obstacleGrid,dp);
-       dp[0][0]=1;
+       //dp[0][0]=1;
+       vector<int>temp(n,0);
+       int l=0;
+       //int ans=0;
        for(int i=0;i<m;i++){
+        vector<int>cur(n,0);
+        l=0;
         for(int j=0;j<n;j++){
+            
             if(obstacleGrid[i][j]==1){
-                dp[i][j]=0;
+                //dp[i][j]=0;
+                cur[j]=0;
+                l=0;
                 continue;
             }
-            if(i==0 &&j==0)continue;
-            int up=0;
-            int left=0;
-            if(i>0) up=dp[i-1][j];
-            if(j>0) left=dp[i][j-1];
-            dp[i][j]=up+left;
+            if(i==0 &&j==0){
+                cur[j]=1;
+                l=1;
+                continue;
+            }
+            
+            int up=temp[j];
+            int left=l;
+            cur[j]=up+left;
+            l=cur[j];
         }
+        temp=cur;
+        
+        
        }
-       return dp[m-1][n-1];
+       return temp[n-1];
        }
 };
