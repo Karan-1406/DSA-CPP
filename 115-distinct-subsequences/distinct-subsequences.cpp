ae529f5682ca4bfc0df27675b4b258005c3535c7
Karan-1406 <karanpandey085@gmail.com>
2026-09-12 14:38:48 +0530

    Time: 11 ms (81.54%) | Memory: 9.1 MB (83.46%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -13,16 +13,19 @@ public:
         int n=s.length();
         int m=t.length();
         // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(m+1,0));
-        for(int i=0;i<=n;i++)dp[i][0]=1;
-        for(int i=1;i<=m;i++)dp[0][i]=0;
+        //vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(m+1,0));
+        vector<unsigned long long>prev(m+1,0),cur(m+1,0);
+        //for(int i=0;i<=n;i++)dp[i][0]=1;
+        //for(int i=1;i<=m;i++)dp[0][i]=0;
         //return solve(n-1,m-1,s,t,dp);
+        prev[0]=cur[0]=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1])dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                else dp[i][j]=dp[i-1][j];
+                if(s[i-1]==t[j-1])cur[j]=prev[j-1]+prev[j];
+                else cur[j]=prev[j];
             }
+            prev=cur;
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
