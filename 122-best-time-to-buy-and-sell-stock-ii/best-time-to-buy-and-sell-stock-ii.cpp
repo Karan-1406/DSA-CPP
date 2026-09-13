30de067c1be2ecbf161d27fa4d0b2901ffee04db
Karan-1406 <karanpandey085@gmail.com>
2026-09-13 21:40:17 +0530

    Time: 8 ms (34.14%) | Memory: 24.7 MB (32.14%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
-    int f(int i,int bought,vector<int>&prices,vector<vector<int>>&dp){
-        if(i==prices.size())return 0;
-        if(dp[i][bought]!=-1)return dp[i][bought];
-        int profit=0;
-        if(!bought){
-             profit=max(-prices[i]+f(i+1,1,prices,dp),f(i+1,0,prices,dp));
+    int f(int i, int bought, vector<int>& prices, vector<vector<int>>& dp) {
+        if (i == prices.size())
+            return 0;
+        if (dp[i][bought] != -1)
+            return dp[i][bought];
+        int profit = 0;
+        if (!bought) {
+            profit = max(-prices[i] + f(i + 1, 1, prices, dp),
+                         f(i + 1, 0, prices, dp));
+        } else {
+            profit = max(prices[i] + f(i + 1, 0, prices, dp),
+                         f(i + 1, 1, prices, dp));
         }
-        else{
-             profit=max(prices[i]+f(i+1,0,prices,dp),f(i+1,1,prices,dp));
-        }
-        return dp[i][bought]=profit;
+        return dp[i][bought] = profit;
     }
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return f(0,0,prices,dp);
+        int n = prices.size();
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        // return f(0,0,prices,dp);
+        dp[n][1] = 0;
+        dp[n][0] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int bought = 0; bought <= 1; bought++) {
+                int profit = 0;
+                if (!bought) {
+                    profit = max(-prices[i] + dp[i+1][1],
+                                 dp[i+1][0]);
+                } else {
+                    profit = max(prices[i] + dp[i+1][0],
+                                 dp[i+1][1]);
+                }
+                dp[i][bought]=profit;
+            }
+        }
+        return dp[0][0];
     }
 };
