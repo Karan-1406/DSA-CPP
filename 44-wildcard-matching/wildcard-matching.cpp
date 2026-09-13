5662c666740273a32c2b606392bd3e2ff5e578fd
Karan-1406 <karanpandey085@gmail.com>
2026-09-13 19:44:42 +0530

    Time: 29 ms (85.42%) | Memory: 11 MB (79.52%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -4,9 +4,9 @@ public:
     //     if(i==0&&j==0)return true;
     //     if(j==0 &&i>0)return false;
     //     if(i==0 &&j>0){
-            // for(int a=1;a<=j;a++){
-            //     if(p[a-1]!='*')return false;
-            // }
+    // for(int a=1;a<=j;a++){
+    //     if(p[a-1]!='*')return false;
+    // }
     //         return true;
     //     }
     // if(dp[i][j]!=-1)return dp[i][j];
@@ -16,32 +16,41 @@ public:
     //     return dp[i][j]=false;
     // }
     bool isMatch(string s, string p) {
-        int n=s.length();
-        int m=p.length();
-        //if(n<m)return false;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        dp[0][0]=true;
-        for(int i=1;i<=n;i++)dp[i][0]=false;
-        for(int j=1;j<=m;j++){
-            int flag=true;
-            for(int a=1;a<=j;a++){
-                if(p[a-1]!='*'){
-                    flag=false;
+        int n = s.length();
+        int m = p.length();
+        // if(n<m)return false;
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        vector<int> prev(m + 1, 0),curr(m+1,1);
+        prev[0] = 1;
+       
+        // for(int i=1;i<=n;i++)dp[i][0]=false;
+        int flag = true;
+        for (int j = 1; j <= m; j++) {
+            bool flag = true;
+            for (int k = 1; k <= j; k++) {
+                if (p[k - 1] != '*') {
+                    flag = false;
                     break;
                 }
             }
-            dp[0][j]=flag;
+            prev[j] = flag;
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1]==p[j-1] || p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
-                else if(p[j-1]=='*')dp[i][j]=dp[i-1][j]||dp[i][j-1];
-                else{
-                    dp[i][j]=0;
+        
+
+        for (int i = 1; i <= n; i++) {
+            curr[0] = false;
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+                    curr[j] = prev[j - 1];
+                else if (p[j - 1] == '*')
+                    curr[j] = prev[j] || curr[j-1];
+                else {
+                    curr[j] = 0;
                 }
             }
+            prev=curr;
         }
-        //return f(n,m,s,p,dp);
-        return dp[n][m];
+        // return f(n,m,s,p,dp);
+        return prev[m];
     }
 };
