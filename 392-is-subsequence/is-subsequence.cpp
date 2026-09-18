27ae1042fb8718a850219ded6e5f1dfc8d7d7780
Karan-1406 <karanpandey085@gmail.com>
2026-09-18 18:58:20 +0530

    Time: 2 ms (6.96%) | Memory: 11.2 MB (5.04%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    bool f(int i,int j,string &s, string&t){
+    bool f(int i,int j,string &s, string&t,vector<vector<int>>&dp){
         //if(i==0)return s[0]==t[j];
         if(i<0)return true;
         if(j<0)return false;
-        if(s[i]==t[j])return f(i-1,j-1,s,t);
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s[i]==t[j])return f(i-1,j-1,s,t,dp);
         
-        return f(i,j-1,s,t);
+        return dp[i][j]=f(i,j-1,s,t,dp);
     }
     bool isSubsequence(string s, string t) {
-        return f(s.length()-1,t.length()-1,s,t);
+        int n=s.length();
+        int m=t.length();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(s.length()-1,t.length()-1,s,t,dp);
     }
 };
